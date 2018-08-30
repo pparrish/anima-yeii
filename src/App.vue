c7d9f5dd76dd0b6d3fa05c678948100886d26ed5
Parrish <paarriisshh@yahoo.com.mx>
2018-08-29 21:26:32 -0500

    Preparado para iniciar las fichas de personaje

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="app">
-    <router-view/>
+      <div class="beta-message" >beta</div>
+      <router-view/>
+      <footer class="footer">
+          <div class="content has-text-centered">
+              <p>Creado por Parrish</p>
+              <p>
+                  <a href="https://bulma.io">
+                    <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" width="128" height="24">
+                  </a>
+              </p>
+          </div>
+      </footer>
   </div>
+
 </template>
 
 <style>
+.beta-message {
+  background: darkorchid;
+  color: whitesmoke;
+  text-align: center;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import CharacterSheet from "@/views/CharacterSheet";
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/character-sheet",
+      name: "character sheet",
+      component: CharacterSheet
     }
   ]
 });
