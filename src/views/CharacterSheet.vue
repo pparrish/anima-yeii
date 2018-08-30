1ea86d9639a408f449102912a00ab3c5ed2cb3fd
Parrish <paarriisshh@yahoo.com.mx>
2018-08-29 21:25:32 -0500

    Pagina de inicio improvisada

---
+++ src/views/CharacterSheet.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <h1 class="title">
+          Ficha de personaje
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "CharacterDetails"
+};
+</script>
+
+<style scoped>
+</style>
