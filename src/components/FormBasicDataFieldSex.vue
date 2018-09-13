49b0332f0952d14aeae02b0e71d8822fe4fcac84
Parrish <paarriisshh@yahoo.com.mx>
2018-09-13 15:11:59 -0500

    Guia de prueba para el  componente

--- src/components/FormBasicDataFieldSex.vue
+++ src/components/FormBasicDataFieldSex.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>
-    $END$
+  <div class="field">
+    <label class="label">Sexo.</label>
+    <div class="buttons has-addons control">
+      <button class="button">Hombre.</button>
+      <button class="button">Mujer.</button>
+    </div>
+    <p class="help is-danger buttons-help">Debes elegir el sexo de tu personaje.</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "FormBasicDataFieldSex"
+  name: "FormBasicDataFieldSex",
+  props: {
+    error: {
+      type: Boolean,
+      default: () => false
+    },
+    sex: {
+      type: String
+    }
+  }
 };
 </script>
 
 <style scoped>
+.buttons-help {
+  margin-top: -1.2rem;
+}
 </style>
