bc1a5c660ad93fa65247472d07aa7ed8c9ade26e
Parrish <paarriisshh@yahoo.com.mx>
2018-09-29 15:51:34 -0500

    Making a random generator with seed and generator types

---
+++ src/components/FromPointsGenerationGenerator.vue
@@ -0,0 +1,46 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: "FromPointGenerationGenerator",
+  props: {
+    seed: {
+      type: String,
+      default: () => "NaNhumano"
+    },
+    type: {
+      type: String,
+      required: true,
+      default: () => "ADVANTAJE"
+    }
+  },
+  methods: {
+    JSF(seed) {
+      function jsf() {
+        var e = s[0] - ((s[1] << 27) | (s[1] >> 5));
+        (s[0] = s[1] ^ ((s[2] << 17) | (s[2] >> 15))),
+          (s[1] = s[2] + s[3]),
+          (s[2] = s[3] + e),
+          (s[3] = s[0] + e);
+        return (s[3] >>> 0) / 4294967295; // 2^32-1
+      }
+      seed >>>= 0;
+      var s = [0xf1ea5eed, seed, seed, seed];
+      for (var i = 0; i < 20; i++) jsf();
+      return jsf;
+    },
+    ADVANTAJE() {
+      let random = this.JSF(this.seed);
+      let values = new Array(8)
+        .fill()
+        .map(() => Math.floor(random() * (11 - 4)) + 4);
+      let minValue = Math.min.apply(null, values);
+      let minValueIndex = values.indexOf(minValue);
+      values[minValueIndex] = 9;
+      return values;
+    }
+  }
+};
+</script>
