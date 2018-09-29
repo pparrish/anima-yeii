392fc99d94d865e6061c07a8ad54aedd0627e584
Parrish <paarriisshh@yahoo.com.mx>
2018-09-29 14:07:09 -0500

    Fix value names for selectors

--- src/components/AccordionSelector.vue
+++ src/components/AccordionSelector.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <p>AccordionSelector</p>
     <accordeon-selector-card 
       v-for="selector in selectors"
       :key="selector.id"
       :title="selector.title"
       :tags="selector.tags"
-      :content="selector.content"
+      :content="selector.description"
       :button-text="selector.buttonText"
       :is-open="selectorOpenend === selector.id"
       :is-selected="selectCard === selector.id"
@@ -21,34 +20,25 @@ import AccordeonSelectorCard from "@/components/AccordionSelectorCard";
 export default {
   name: "AccordeonSelector",
   components: { AccordeonSelectorCard },
+  model: {
+    prop: "selectCard",
+    event: "input"
+  },
   props: {
     selectors: {
       type: Array,
       required: false,
-      default: () => {
-        return [
-          {
-            id: 1,
-            title: "Prueba",
-            tags: ["prueba", "pruebita"],
-            content: "Esto es una prueba yeahh",
-            buttonText: "Clica la prueba"
-          },
-          {
-            id: 2,
-            title: "Prueba",
-            tags: ["prueba", "pruebita"],
-            content: "Esto es una prueba yeahh",
-            buttonText: "Clica la prueba"
-          }
-        ];
-      }
+      default: () => []
+    },
+    selectCard: {
+      type: String,
+      required: false,
+      default: () => undefined
     }
   },
   data() {
     return {
-      selectorOpenend: undefined,
-      selectCard: undefined
+      selectorOpenend: undefined
     };
   },
   methods: {
@@ -56,9 +46,8 @@ export default {
       this.selectorOpenend = this.selectorOpenend !== id ? id : undefined;
     },
     selectedCard(id) {
-      this.selectCard = id;
       this.toggleCards(id); //this close all cards
-      this.$emit("input", this.selectCard);
+      this.$emit("input", id);
     }
   }
 };
--- src/components/FormPointsGenerationSelector.vue
+++ src/components/FormPointsGenerationSelector.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <p>FormPointsGenerationSelector</p>
-    <accordion-selector />
+    <accordion-selector 
+      :selectors="generationCards"
+      v-model="selectedCard"/>
   </div>
 </template>
 
@@ -10,7 +11,69 @@ import AccordionSelector from "@/components/AccordionSelector";
 
 export default {
   name: "FormPointsGenerationSelector",
-  components: { AccordionSelector }
+  components: { AccordionSelector },
+  props: {
+    value: {
+      type: String,
+      required: false,
+      default: () => undefined
+    }
+  },
+  data() {
+    return {
+      generationCards: [
+        {
+          id: "ADVANTAGE",
+          title: "Al azar con ventaja.",
+          tags: ["recomendado"],
+          buttonText: "Selecciona este método",
+          description:
+            "Se te darán 8 números al azar del 4 al 10 con la seguridad de uno de ellos siempre será 9. "
+        },
+        {
+          id: "RISK",
+          title: "Al azar con riesgo.",
+          tags: ["Cifras bajas"],
+          buttonText: "Selecciona este método",
+          description:
+            "Se obtiene con 8 pares de números del 1 al 10 de los cuales se elije solo el mayor. Corres el riesgo de obtener vaores muy bajos."
+        },
+        {
+          id: "RANDOM",
+          title: "Completamente al azar.",
+          tags: ["Para NPC´s"],
+          buttonText: "Selecciona este método",
+          description:
+            "8 números del 1 al 10, completamente al azar. Ideal para los personajes comunes y corrientes"
+        },
+        {
+          id: "POINTS_RANDOM",
+          title: "Puntos al azar.",
+          buttonText: "Selecciona este método",
+          tags: ["De 7 a 70"],
+          description:
+            "Se obtiene de la suma de 7 números del 1 al 10 y después puedes repartir esos puntos como gustes. "
+        },
+        {
+          id: "POINTS",
+          title: "Por puntos.",
+          tags: ["Libre"],
+          buttonText: "Selecciona este método",
+          description:
+            "Eliges la cantidad de puntos a repartir, por defecto son 60."
+        }
+      ],
+      selectedCard: undefined
+    };
+  },
+  watch: {
+    selectedCard(selected) {
+      this.$emit("input", selected);
+    },
+    value() {
+      this.selectedCard = this.value;
+    }
+  }
 };
 </script>
 
