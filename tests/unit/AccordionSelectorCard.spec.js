7b4a17c56451fa27427879d1e04a58a981961bc9
Parrish <paarriisshh@yahoo.com.mx>
2018-09-30 01:03:33 -0500

    Borrando componentes basura

--- tests/unit/AccordionSelectorCard.spec.js
+++ tests/unit/AccordionSelectorCard.spec.js
@@ -1,24 +1,75 @@
-import Vue from "vue";
+import { shallowMount } from "@vue/test-utils";
 import AccordionSelectorCard from "@/components/AccordionSelectorCard";
 
 describe("AccordionSelectorCard.vue", () => {
-  let cmp, vm;
+  let cmp;
+
   beforeEach(() => {
-    cmp = Vue.extend(AccordionSelectorCard); //CReate a copy or original component
-    vm = new cmp({}).$mount(); //if you whant remplase data with fake and mount
+    cmp = shallowMount(AccordionSelectorCard);
   });
-  describe("All default values is set correctly", () => {
+
+  it("has the expected html structure", () => {
+    expect(cmp.vm.$el).toMatchSnapshot();
+  });
+
+  describe("All default values is set default correctly", () => {
     it("default title must be 'No title'", () => {
-      expect(vm.title).toEqual("No title");
+      expect(cmp.vm.title).toEqual("No title");
     });
     it("default tagg must be []", () => {
-      expect(vm.tags).toEqual([]);
+      expect(cmp.vm.tags).toEqual([]);
     });
     it("default content must be 'No content'", () => {
-      expect(vm.content).toEqual("No content");
+      expect(cmp.vm.content).toEqual("No content");
     });
     it("default title must be 'No text'", () => {
-      expect(vm.buttonText).toEqual("No text");
+      expect(cmp.vm.buttonText).toEqual("No text");
+    });
+  });
+
+  describe("Props must be  set correctly", () => {
+    it("Title must be 'Selector Card'", () => {
+      cmp = shallowMount(AccordionSelectorCard, {
+        propsData: {
+          title: "Selector Card"
+        }
+      });
+
+      expect(cmp.vm.title).toBe("Selector Card");
+    });
+
+    it("tags must be ['tag', 'a tag']", () => {
+      cmp = shallowMount(AccordionSelectorCard, {
+        propsData: {
+          tags: ["tag", "a tag"]
+        }
+      });
+
+      expect(cmp.vm.tags).toEqual(["tag", "a tag"]);
+    });
+
+    it("content must be 'A content'", () => {
+      cmp = shallowMount(AccordionSelectorCard, {
+        propsData: {
+          content: "A content"
+        }
+      });
+
+      expect(cmp.vm.content).toEqual("A content");
+    });
+
+    it("Button must be say 'Select me'", () => {
+      cmp = shallowMount(AccordionSelectorCard, {
+        propsData: {
+          buttonText: "Select me"
+        }
+      });
+
+      expect(cmp.vm.buttonText).toEqual("Select me");
+    });
+
+    it("Haz the espected structure", () => {
+      expect(cmp.vm.$el).toMatchSnapshot();
     });
   });
 });
