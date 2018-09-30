ff2bd46d8c18e1e8b3321762eb78ce013af99211
Parrish <paarriisshh@yahoo.com.mx>
2018-09-29 22:51:22 -0500

    Add a input for the individual points

---
+++ src/components/FormPointsGenerationInputPoints.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="columns is-mobile is-variable is-1">
+    <div class="column"><button 
+      class="button is-warning"  
+      @click="handleInput(value-10)">-10</button></div>
+    <div class="column is-half">
+      <input
+        :value="value"
+        type="number"
+        class="input"
+        @input="handleInput($event.target.value)">
+    </div>
+    <div class="column"><button 
+      class="button is-info" 
+      @click="handleInput(value+10)">+10</button></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "FormPointsGenerationInputPoints",
+  props: {
+    value: {
+      type: Number,
+      default: () => 0,
+      required: true
+    }
+  },
+  methods: {
+    handleInput(value) {
+      if (value < 0) value = 0;
+      this.$emit("input", value);
+    }
+  }
+};
+</script>
